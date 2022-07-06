f8d6512d651b19427e0dab8eeb8878d5c0d52d89
Facundo Lazarte <facundolazarte95@outlook.com>
2022-07-05 21:34:34 -0300

    entrega2

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,14 +1,14 @@
-function bucle(){
+// function bucle(){
 
-    var num=prompt("ingrese un numero",);
+//     var num=prompt("ingrese un numero",);
 
-     for (let index = 0; index < num; index++) {
+//      for (let index = 0; index < num; index++) {
 
-        console.log("hola "+index);
+//         console.log("hola "+index);
             
-     }
+//      }
     
-}
+// }
 
 // function bucle(){
 //     var con="";
@@ -20,4 +20,16 @@ function bucle(){
 //     }
 // }
 
-window.onload=bucle();
+// window.onload=bucle();
+
+function calcular() {
+
+   var t1=document.getElementById("texto1").value; 
+   var t2=document.getElementById("texto2").value;
+   var t3=document.getElementById("texto3").value;
+   var t4=document.getElementById("texto4").value;
+   var t5=document.getElementById("texto5").value;
+
+   document.getElementById("resultado").innerHTML=(parseInt(t1)+parseInt(t2)+parseInt(t3)+parseInt(t4)+parseInt(t5))*1.21;
+
+}
